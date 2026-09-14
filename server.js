51d74cdd2ae9c7309ab48c306c6d129441a37f80
conname <hiiimynameisanon@gmail.com>
2026-09-14 19:17:47 +0800

    Dummy database and GET method

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const app = express();
 
+const users = require("./users.js"); //IMPORT dummy database
+
 app.use(express.json()); // IMPORTANT: parse JSON body
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
-})
+});
+
+app.get("/", (req, res) => {
+    res.json(users);
+});
+
+// post request
+app.post("/users", (req, res) =>{
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+        return res.status(400).json({
+            message: "Name and email are required",
+        });
+    }
+
+    const newUser ={
+    id: users.length +1,
+    name,
+    email,
+    };
+
+    users.push(newUser);
+
+    res.status(201).json(newUser);
+});
---
+++ users.js
@@ -0,0 +1,6 @@
+let users = [
+    { id: 1, name: "John", email: "john@email.com"},
+    { id: 2, name: "Jane", email: "jane@email.com"}
+];
+
+module.exports = users;
